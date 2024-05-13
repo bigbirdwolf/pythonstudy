228ca301d25e8cd9683815bf6303d91e845919bc
wolf <big_bird_wolf@163.com>
2024-05-13 20:50:18 +0800

    WebEngineWidget

---
+++ PyQt/QWebEngineWidget/WebEngineDemo.py
@@ -0,0 +1,28 @@
+import sys
+
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtGui import *
+
+class WebEngineViewDemo(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('QtWebEngineView Demo')
+        self.setGeometry(100, 100, 800, 600)
+
+        try:
+            # 创建一个 QWebEngineView 实例
+            webview = QWebEngineView()
+            webview.load(QUrl('https://www.example.com'))  # 加载网页
+            self.setCentralWidget(webview)
+
+        except Exception as e:
+            print(e)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = WebEngineViewDemo()
+    window.show()
+    sys.exit(app.exec_())
